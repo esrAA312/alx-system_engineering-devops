fbe327a7c83c089f3305d946a4788f0f35fb6d11
esrAA312 <esraayahya312@gmail.com>
2024-03-25 15:27:08 -0700

    done

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,24 @@
-package { 'nginx':
-	  ensure => installed,
-}
+# install and set up nginx
 
-file_line { 'redirect':
-	  ensure => present,
-		   path   => '/etc/nginx/sites-enabled/default',
-		     line   => 'rewrite ^/redirect_me https://www.github.com/besthor permanent;',
-		       after  => 'listen 80 default_server;',
+package { 'nginx':
+	ensure => 'installed',
 }
 
 file { '/var/www/html/index.html':
-	  ensure  => file,
-		    content => 'Hello World!',
+	require => Package['nginx'],
+	content => 'Hello World!',
+}
+
+file_line { 'add-rewrite':
+	ensure  => 'present',
+		require => Package['nginx'],
+		path    => '/etc/nginx/sites-available/default',
+		after   => 'root /var/www/html;',
+		line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH@-TGUlwu4 permanent;',
+		notify  => Service['nginx'],
 }
 
 service { 'nginx':
-	  ensure  => running,
-		    enable  => true,
-		      require => Package['nginx'],
+	ensure  => running,
+		require => File_line['add-rewrite'],
 }
