df4e2915124f96628d45996878365325bb1fa604
esrAA312 <esraayahya312@gmail.com>
2024-03-29 07:43:09 -0700

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,26 @@
-#puppet
+#pp
 exec { 'update':
-	command  => 'sudo apt-get update',
-		 provider => shell,
+	command  => 'apt-get update',
+		 user     => 'root',
+		 path     => '/usr/bin:/usr/sbin:/bin',
 }
--> package {'nginx':
-	ensure => present,
+
+package { 'nginx':
+	ensure  => 'installed',
+		require => Exec['update'],
 }
--> file_line { 'header line':
-	ensure => present,
-	       path   => '/etc/nginx/sites-available/default',
-	       line   => "        location / {
-		       add_header X-Served-By ${hostname};",
-		       match  => '^\tlocation / {',
-		       }
-		       -> exec { 'restart service':
-			       command  => 'sudo service nginx restart',
-			       provider => shell,
-		       }
+
+file { '/etc/nginx/sites-available/default':
+	ensure  => 'file',
+		content => "\
+			 location / {
+				 add_header X-Served-By ${hostname};
+				 ",
+					 require => Package['nginx'],
+			 }
+
+	service { 'nginx':
+		ensure    => 'running',
+		enable    => true,
+		subscribe => File['/etc/nginx/sites-available/default'],
+	}
