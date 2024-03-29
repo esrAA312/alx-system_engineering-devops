40b434e65656c94f5f24328806670d7624034451
esrAA312 <esraayahya312@gmail.com>
2024-03-29 07:48:08 -0700

    done

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,26 +1,55 @@
-#pp
-exec { 'update':
-	command  => 'apt-get update',
-		 user     => 'root',
-		 path     => '/usr/bin:/usr/sbin:/bin',
+etup New Ubuntu server with nginx
+# and add a custom HTTP header
+
+exec { 'update system':
+	command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
 	ensure  => 'installed',
-		require => Exec['update'],
+		require => Exec['update system'],
 }
 
-file { '/etc/nginx/sites-available/default':
-	ensure  => 'file',
-		content => "\
-			 location / {
-				 add_header X-Served-By ${hostname};
-				 ",
-					 require => Package['nginx'],
-			 }
+file { '/var/www/html/index.html':
+	content => 'Hello World!',
+}
 
-	service { 'nginx':
-		ensure    => 'running',
-		enable    => true,
-		subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'configure nginx':
+	command  => 'sed -i "24i\        rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default && \
+		  sed -i "25i\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+		 provider => 'shell',
+		 require  => Package['nginx'],
+		 notify   => Service['nginx'],
+}
+
+service { 'nginx':
+	ensure    => 'running',
+		  enable    => true,
+		  require   => Package['nginx'],
+}
+
+# Alternative approach using Puppet's built-in resources
+exec { 'update':
+	command  => 'apt-get update',
+		 path     => '/usr/bin:/usr/sbin:/bin',
+}
+
+package { 'nginx':
+	ensure   => 'present',
+		 require  => Exec['update'],
+}
+
+file_line { 'header line':
+	ensure => present,
+	       path   => '/etc/nginx/sites-available/default',
+	       line   => "        location / {\n  add_header X-Served-By ${hostname};",
+	       match  => '^\s*location / {',
+		       require => Package['nginx'],
+	       }
+
+	exec { 'restart service':
+		command  => 'service nginx restart',
+		path     => '/usr/bin:/usr/sbin:/bin',
+		provider => shell,
+		require  => Package['nginx'],
 	}
